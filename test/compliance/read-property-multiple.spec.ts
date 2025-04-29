6b387442a86d71407fb97122a48b4bf295e80c16
mandrake-sw <zanetti.sw@gmail.com>
2025-04-29 11:25:37 +0200

    fix: add port configuration to bacnetClient instantiation in compliance test

--- test/compliance/read-property-multiple.spec.ts
+++ test/compliance/read-property-multiple.spec.ts
@@ -16,6 +16,7 @@ test.describe('bacnet - read property multiple compliance', () => {
 			bacnetClient = new utils.bacnetClient({
 				apduTimeout: utils.apduTimeout,
 				interface: utils.clientListenerInterface,
+				port: 47809,
 				reuseAddr: true,
 			})
 			bacnetClient.on('message', (msg: any, rinfo: any) => {
